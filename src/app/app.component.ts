7fb709183daeb9a9aebdc82cca305cd578fc1577
mmullerx4 <mmullerx4@gmail.com>
2024-11-06 21:47:46 -0500

    w08-tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  @ViewChild('signupForm', { static: false}) sgnForm: NgForm;
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 
 }
